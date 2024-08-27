d1276950c52ad6f592b545772551a9b543c63364
Søilad <joshtgm@icloud.com>
2024-08-27 22:34:34 +0530

    Update p15.py

--- p15.py
+++ p15.py
@@ -1,5 +1,7 @@
-def main(l,i = input('give a list (separate with commas): ').split(','),input('enter element to search'))
+def main(l,i = input('enter element to search')):
     print(f'{i} is in {l}') if i in l else print(f'{i} is not in {l}')
 
 if __name__ == '__main__':
-    main()
+    l,t = ['a','s','d','f'],('a','s','d','f')
+    main(l)
+    main(t)
