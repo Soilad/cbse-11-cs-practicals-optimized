b7ab77f1612a8c3e8fc83b78e21cfc6cafce7a05
Søilad <joshtgm@icloud.com>
2024-08-27 22:48:15 +0530

    Update p15.py

--- p15.py
+++ p15.py
@@ -1,4 +1,4 @@
-def main(l,i = input('enter element to search')):
+def main(l,i = input('enter element to search: ')):
     print(f'{i} is in {l}') if i in l else print(f'{i} is not in {l}')
 
 if __name__ == '__main__':
