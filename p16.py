5085f9d7922df025fc6d203abe217b5a24977871
Søilad <joshtgm@icloud.com>
2024-08-27 22:47:58 +0530

    Update p16.py

--- p16.py
+++ p16.py
@@ -1,7 +1,6 @@
 def main(l):
-    while ['exit'] not in l:
-        l.append(input('give students detail in the form of name,roll no.,mark (type exit to exit): ').split(','))
-    l.remove(['exit'])
+    while ['0','0','0'] not in l:
+        l.append(input('give students detail in the form of name,roll no.,mark (type 0,0,0 to exit): ').split(','))
     print(*list(filter(lambda x: (int(x[2])>75),l)))
 
 if __name__ == '__main__':
