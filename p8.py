821966709b8ce1808b8893ada3d909adbf3bbf49
Søilad <joshtgm@icloud.com>
2024-08-29 15:07:20 +0500

    Update p8.py

--- p8.py
+++ p8.py
@@ -1,7 +1,7 @@
 def main(l = 0):
     n = int(input('give number: '))
     for i in range(2,n):
-        if n%i == 0:
+        if not n%i:
             l = 1
             break
     print(f'{n} is prime') if l == 0 else print(f'{n} is compsite')
