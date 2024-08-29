37d30f827fa44670ad2193220d96987b48cb76bf
Søilad <joshtgm@icloud.com>
2024-08-29 19:24:21 +0500

    Update p8.py
    implemented better prime detection algorithm

--- p8.py
+++ p8.py
@@ -1,10 +1,10 @@
-def main(l = 0):
+def main(l):
     n = int(input('give number: '))
-    for i in range(2,n):
+    for i in list(range(3,round(n**0.5),2))+[2]:
         if not n%i:
-            l = 1
+            l=1
             break
     print(f'{n} is prime') if l == 0 else print(f'{n} is compsite')
 
 if __name__ == '__main__':
-    main()
+    main(0)
