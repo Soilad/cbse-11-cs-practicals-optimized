da5b2ff35ac40e6ca29cfcf18fe1a14c355b5122
Søilad <joshtgm@icloud.com>
2024-09-02 00:37:30 +0530

    Update p11.py

--- p11.py
+++ p11.py
@@ -5,7 +5,7 @@ def main(str = input('give string: ')):
     for i in vowels:
         vcount += str.lower().count(i)
     for j in set(str):
-        acount += str.count(j) if not j.islower() else 0
+        acount += str.count(j) if j.isupper() else 0
     ccount,lcount = length - vcount,length - acount
     print(f'number of vowels: {vcount} \nnumber of consonants {ccount} \nnumber of uppercase letters {acount} \nnumber of lowercase letters {lcount}')
 
